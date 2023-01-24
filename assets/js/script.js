852940d17ce770aa7749a54fc62c92215ae8de45
MarKubil <marius.kub@outlook.com>
2023-01-24 11:56:58 +0000

    Finished loading data from localStorage to text blocks, save button works to save input to localStorage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,40 @@ currentDay.text(today.format("dddd, Do MMMM YYYY"));
 
 // Variable to access the time blocks on the page 
 var timeBlocks = $(".time-block");
+
+// Variable to access the current time.
+var currentTime = new Date().getHours();
+
+// Variables to store user input into local storage.
+var textAreas = $(".description");
+var saveBtn = $(".saveBtn");
+
+
+// Function to access all elements that have a class "time-block".
+timeBlocks.each(function() {
+    // 
+    var time = parseInt($(this).attr("data-time"));
+
+    // Changes block colors by current time.
+    if(time < currentTime) {
+        $(this).addClass("past");
+    } else if (time === currentTime) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass('future');
+    }
+});
+
+// Load data stored in localStorage.
+textAreas.each(function(index) {
+    var key = "textArea" + index;
+    $(this).val(localStorage.getItem(key));
+});
+
+// Button to save user input.
+saveBtn.on("click", function() {
+    var textA = $(this).siblings(".description");
+    var areaIndex = textAreas.index(textA);
+    var key = "textArea" + areaIndex;
+    localStorage.setItem(key, textA.val());
+});
